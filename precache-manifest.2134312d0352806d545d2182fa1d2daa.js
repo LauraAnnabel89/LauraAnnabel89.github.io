d4c0564a0df8030b8c1921a541fbc83851113ff2
Laura-Annabel Tombs <laura.tombs@gmail.com>
2020-03-17 11:40:18 +0000

    Add files via upload

---
+++ precache-manifest.2134312d0352806d545d2182fa1d2daa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa5503b05063e9adcc5f66802351fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4eb13c5ad36f267ebca",
+    "url": "/static/css/main.4fe9b326.chunk.css"
+  },
+  {
+    "revision": "a03c83d2ae666b7bc4c9",
+    "url": "/static/js/2.c91f6995.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.c91f6995.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4eb13c5ad36f267ebca",
+    "url": "/static/js/main.635d46ba.chunk.js"
+  },
+  {
+    "revision": "23ab70846ff2ed365202",
+    "url": "/static/js/runtime-main.19e5cc06.js"
+  },
+  {
+    "revision": "d26b93536f978befea0959f1245a6aea",
+    "url": "/static/media/cp-logo-wht.d26b9353.png"
+  },
+  {
+    "revision": "4773325a38cc5b0a85c80cf5c5b4ecc1",
+    "url": "/static/media/cp-text.4773325a.png"
+  }
+]);
